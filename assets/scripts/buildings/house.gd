48fe6d2649f730e3800bdf24776c8b4951916ad9
levalyukov <lev.alyukov@gmail.com>
2026-09-14 20:22:20 +0500

    formatting

--- assets/scripts/buildings/house.gd
+++ assets/scripts/buildings/house.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-@onready var cycle = get_tree().current_scene.cycle
 @onready var cycle: WorldCycle = get_tree().current_scene.cycle
 @onready var collision: Area2D = $Collision
 @onready var sprite: Sprite2D = $Sprite2D
